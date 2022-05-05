99166e13bf1d1e42a851280e4a03fcb793bfc5f2
Giorgio <giomonline@gmail.com>
2022-05-05 12:33:13 +0200

    test: update LQ tests

--- experiments/learning/suite.py
+++ experiments/learning/suite.py
@@ -17,9 +17,10 @@ class MySuite(PyExperimentSuite):
         seed_all_agent(self.seed)
 
         # Environment
-        if params['path'] == 'results_lq':
-            self.env = LQ(1,1,max_pos=10, max_action = float('inf'), sigma_noise=params['sigma_noise'])
-
+        if params['path'] == 'results_lq_s1':
+            self.env = LQ(1,1,max_pos=50, max_action = float('inf'), sigma_noise=params['sigma_noise'])
+        elif params['path'] == 'results_lq_s5':
+            self.env = LQ(5,1,max_pos=50, max_action = float('inf'), sigma_noise=params['sigma_noise'])
         elif params['path'] == 'results_cartpole':
             self.env = gym.make('ContCartPole-v0')
         else:
