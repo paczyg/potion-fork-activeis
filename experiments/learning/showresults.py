04493a755ac3893e54c5644a8cbba0a0dd3e5ca4
Giorgio <giomonline@gmail.com>
2022-10-23 12:29:03 +0200

    test: new LQ tests

--- experiments/learning/showresults.py
+++ experiments/learning/showresults.py
@@ -1,83 +1,168 @@
+#%%
 from suite import MySuite
 
 import numpy as np
+import pandas as pd
 import scipy.stats as st
 from matplotlib import pyplot as plt
 
-mysuite = MySuite(config='lq.cfg')
+def get_dataframes_over_repetitions(suite, exp, tags="all"):
+    # Retrieve the actual repetitions of a given experiment tags
+    # If a repetition has less iterations than the prescibed ones, NaN values are used to fill the dataframe
 
-# sigma 0
-exp_off = mysuite.get_exps(path='results_lq/offpolicy/sigma_noise0.0')[0]
-params = mysuite.get_params(exp_off)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='off policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+    dfs = []
+    params = suite.get_params(exp)
+    for rep in range(params['repetitions']):
+        log = suite.get_history(exp, rep, tags)
+        if len(log) == 0:
+            continue
+        if tags != "all" and type(tags) is not list:
+            log = pd.DataFrame(log,columns=[tags])
+        else:
+            log = pd.DataFrame(log)
+        # Fill rows with the expected number of iterations
+        if log.shape[0] < params["iterations"]:
+            log = pd.concat([log,pd.DataFrame(np.nan, index=np.arange(params["iterations"]-log.shape[0]), columns=log.columns)])
+        dfs.append(log)
+    return dfs
 
-exp_on = mysuite.get_exps(path='results_lq/onpolicy/sigma_noise0.0')[0]
-params = mysuite.get_params(exp_on)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='on policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+def get_ci(suite, exp, key):
+    params = suite.get_params(exp)
+    n = params['repetitions']
+    m = suite.get_histories_over_repetitions(exp, key, np.mean)
+    s = suite.get_histories_over_repetitions(exp, key, np.std)
+    s[s==0] = "nan"
+    lb, ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
+    return m, lb, ub
 
-plt.xlabel('Itrations')
+def plot_ci(suite, exp, key="UTestPerf", label=None):
+    params = suite.get_params(exp)
+    m, ci_lb, ci_ub = get_ci(suite, exp, key)
+    plt.plot(np.arange(params['iterations']), m, label=label)
+    plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+
+#%% ===================================================================================
+mysuite = MySuite(config='lq_s1.cfg')
+main_dir = "results_lq_s1"
+
+exp_off  = f"{main_dir}/offpolicy/batchsize{15}defensive_batch{5}"
+exp_on  = f"{main_dir}/onpolicy/batchsize{20}"
+
+plt.ylabel('Deterministic Performance')
 plt.ylabel('Return')
+plt.xlabel('Trajectories')
+
+m, ci_lb, ci_ub = get_ci(mysuite, exp_on, 'TestPerf')
+plt.plot(np.cumsum(mysuite.get_history(exp_on,0,'BatchSize')), m, label='onpolicy')
+plt.fill_between(np.cumsum(mysuite.get_history(exp_on,0,'BatchSize')), ci_lb, ci_ub, alpha=.1)
+
+m, ci_lb, ci_ub = get_ci(mysuite, exp_off, 'TestPerf')
+plt.plot(np.cumsum(mysuite.get_history(exp_off,0,'Batch_total')), m, label='offpolicy')
+plt.fill_between(np.cumsum(mysuite.get_history(exp_off,0,'Batch_total')), ci_lb, ci_ub, alpha=.1)
+
 plt.legend()
-plt.title("Sigma noise = 0")
 plt.show()
 
+#%% === LQ 1 11Agosto================================================================================
+mysuite = MySuite(config='lq_s1.cfg')
+main_dir = "results_lq_s1_11Agosto"
 
-# sigma 0.5
-exp_off = mysuite.get_exps(path='results_lq/offpolicy/sigma_noise0.50')[0]
-params = mysuite.get_params(exp_off)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='off policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+stepper      = "ConstantStepper1e4" #"ConstantStepper1e4", "Adam1e1", "Adam1e2", "Adam1e4"
+horizon      = "10"                 #10, 50
+sigma_noise  = "0"                  #0, 2
+mu_init      = "0"                  #0, 0.5, 1
+logstd_init  = "30"                 #00, 10, 20, 30
 
-exp_on = mysuite.get_exps(path='results_lq/onpolicy/sigma_noise0.50')[0]
-params = mysuite.get_params(exp_on)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='on policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+exp_on  = f"{main_dir}/onpolicy/sigma_noise{sigma_noise}stepper{stepper}horizon{horizon}mu_init{mu_init}logstd_init{logstd_init}"
+exp_off  = f"{main_dir}/offpolicy/sigma_noise{sigma_noise}stepper{stepper}horizon{horizon}mu_init{mu_init}logstd_init{logstd_init}"
 
+plt.ylabel('Deterministic Performance')
 plt.xlabel('Itrations')
-plt.ylabel('Return')
+plot_ci(mysuite, exp_on, label="onpolicy")
+plot_ci(mysuite, exp_off, label="offpolicy")
+plt.title(f"stepper={stepper}, horizon={horizon}, sigma_noise={sigma_noise}, mu_init={mu_init}, logstd_init={logstd_init}")
 plt.legend()
-plt.title("Sigma noise = 0.5")
 plt.show()
 
-# sigma 1
-exp_off = mysuite.get_exps(path='results_lq/offpolicy/sigma_noise1.0')[0]
-params = mysuite.get_params(exp_off)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_off, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='off policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+# Query behavioural policy parameters
+# fig,ax = plt.subplots()
+# ax.plot(
+#     mysuite.get_histories_fix_params('{main_dir}/sigma_noise0', 0, 'ce_policy_loc00', n_offpolicy_opt=1)[0][0],
+#     color='red'
+# )
+# ax.set_ylabel('ce policy loc', color='red')
+# ax2=ax.twinx()
+# ax2.plot(
+#     mysuite.get_histories_fix_params('{main_dir}/sigma_noise0', 0, 'param0', n_offpolicy_opt=1)[0][0],
+#     color='blue'
+# )
+# ax2.set_ylabel('target policy loc', color='blue')
+# plt.xlabel('Itrations')
+# plt.title(f"Sigma noise = 0")
+# plt.show()
 
-exp_on = mysuite.get_exps(path='results_lq/onpolicy/sigma_noise1.0')[0]
-params = mysuite.get_params(exp_on)
-n = params['repetitions']
-m = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.mean)
-s = mysuite.get_histories_over_repetitions(exp_on, 'UTestPerf', np.std)
-ci_lb, ci_ub = st.norm.interval(0.68, loc=m, scale=s/np.sqrt(n))
-plt.plot(np.arange(params['iterations']), m, label='on policy')
-plt.fill_between(np.arange(params['iterations']), ci_lb, ci_ub, alpha=.1)
+# plt.plot(
+#     mysuite.get_histories_fix_params('{main_dir}/sigma_noise0', 0, 'ce_policy_scale00', n_offpolicy_opt=1)[0][0]
+# )
+# plt.xlabel('Itrations')
+# plt.ylabel('ce policy scale')
+# plt.title("Sigma noise = 0")
+# plt.show()
 
+
+#%% === LQ 5 ================================================================================
+mysuite = MySuite(config='lq_s5.cfg')
+
+stepper      = "Adam1e4" #"ConstantStepper1e4", "Adam1e1", "Adam1e2", "Adam1e4"
+horizon      = "10" #10, 50
+sigma_noise  = "0" #0, 2
+mu_init      = "0" #0, 05, 1
+logstd_init  = "10"
+
+exp_on  = f"results_lq_s5/onpolicy"
+exp_off  = f"results_lq_s5/offpolicy"
+
+# plt.subplot(2, 1, 1)
+plot_ci(mysuite, exp_on, label="onpolicy")
+plot_ci(mysuite, exp_off, label="offpolicy")
+plt.ylabel('Deterministic Performance')
 plt.xlabel('Itrations')
-plt.ylabel('Return')
+# plt.title(f"stepper={stepper}, horiz  on={horizon}, sigma_noise={sigma_noise}, mu_init={mu_init}")
 plt.legend()
-plt.title("Sigma noise = 1")
 plt.show()
+
+# ax = plt.subplot(2, 1, 2)
+# plot_ci(mysuite, exp_on, key="VarMean", label="onpolicy 1000")
+# plot_ci(mysuite, exp_off, key="VarMean", label="offpolicy 500+500")
+# plt.ylabel('Variance of the gradient')
+# plt.xlabel('Itrations')
+# plt.title(f"stepper={stepper}, horizon={horizon}, sigma_noise={sigma_noise}, mu_init={mu_init}")
+# ax.set_yscale('log')
+# plt.legend()
+# plt.show()
+
+
+#%% === CARTPOLE ================================================================================
+mysuite = MySuite(config='cartpole.cfg')
+
+logstd_init  = "10" #"10", "30"
+exp_on   = f"results_cartpole_0818/onpolicy/logstd_init{logstd_init}"
+exp_off   = f"results_cartpole_0818/offpolicy/logstd_init{logstd_init}"
+
+# plt.subplot(2, 1, 1)
+plot_ci(mysuite, exp_on, label="onpolicy")
+plot_ci(mysuite, exp_off, label="offpolicy")
+plt.ylabel('Deterministic Performance')
+plt.xlabel('Itrations')
+plt.legend()
+plt.show()
+
+# ax = plt.subplot(2, 1, 2)
+# plot_ci(mysuite, exp_on, key="VarMean", label="onpolicy 200")
+# plot_ci(mysuite, exp_off, key="VarMean", label="offpolicy 100+100")
+# plt.ylabel('Variance of the gradient')
+# plt.xlabel('Itrations')
+# plt.title(f"stepper={stepper}")
+# ax.set_yscale('log')
+# plt.legend()
+# plt.show()
--- experiments/learning/suite.py
+++ experiments/learning/suite.py
@@ -7,7 +7,9 @@ from potion.envs.lq import LQ
 from potion.common.misc_utils import clip, seed_all_agent
 from potion.meta.steppers import ConstantStepper, Adam
 from potion.actors.continuous_policies import ShallowGaussianPolicy
-from potion.algorithms.reinforce_step import reinforce_step
+from potion.algorithms.reinforce_offpolicy import reinforce_offpolicy_step
+from potion.algorithms.reinforce import reinforce_step
+from potion.simulation.trajectory_generators import generate_batch
 
 class MySuite(PyExperimentSuite):
 
@@ -17,6 +19,7 @@ class MySuite(PyExperimentSuite):
         seed_all_agent(self.seed)
 
         # Environment
+        # ===========
         if params['path'] == 'results_lq_s1':
             self.env = LQ(1,1,max_pos=10, max_action = float('inf'), sigma_noise=params['sigma_noise'], horizon=params["horizon"])
         elif params['path'] == 'results_lq_s5':
@@ -25,11 +28,13 @@ class MySuite(PyExperimentSuite):
             self.env = gym.make('ContCartPole-v0')
         else:
             raise NotImplementedError
+        self.env.horizon = params['horizon']
         self.env.seed(self.seed)
         state_dim  = sum(self.env.observation_space.shape)
         action_dim = sum(self.env.action_space.shape)
 
         # Policy
+        # ======
         self.policy = ShallowGaussianPolicy(
             state_dim, # input size
             action_dim, # output size
@@ -40,37 +45,56 @@ class MySuite(PyExperimentSuite):
         
         self.stepper = eval(params["stepper"])
 
-    def iterate(self, params, rep, n):
-        if isinstance(params['ce_batchsizes'], str):
-            ce_batchsizes = eval(params['ce_batchsizes'])
-        else:
-            ce_batchsizes = params['ce_batchsizes']
+        # Algorithm
+        # =========
+        if 'offpolicy/' in params['name']:
+            # Initial data for first offline CE estimation
+            if params['ce_use_offline_data']:
+                self.offline_policies = [self.policy]
+                self.offline_batches = [generate_batch(self.env, self.policy, self.env.horizon, params['batchsize'], seed=self.seed)]
+            else:
+                self.offline_policies = None
+                self.offline_batches = None
 
-        log = reinforce_step(
-            self.env, self.policy, params['horizon'],
-            ce_batchsizes       = ce_batchsizes,
-            estimator           = params['estimator'],
-            baseline            = params['baseline'],
-            batchsize           = params['batchsize'],
-            test_batchsize      = 500,
-            action_filter       = params['action_filter'],
-            defensive           = params['defensive'],
-            biased_offpolicy    = params['biased_offpolicy'],
-            disc                = self.env.gamma,
-            stepper             = self.stepper,
-            seed                = self.seed+n,
-            log_params          = True,
-            log_grad            = True,
-            log_ce_params       = True,
-            estimate_var        = True,
-            shallow             = True
-        )
+    def iterate(self, params, rep, n):
+        # Offpolicy algorithm
+        if 'offpolicy' in params['name']:
+            if isinstance(params['ce_batchsizes'], str):
+                ce_batchsizes = eval(params['ce_batchsizes'])
+            else:
+                ce_batchsizes = params['ce_batchsizes']
+
+            log, self.offline_policies, self.offline_batches = reinforce_offpolicy_step(
+                self.env, self.policy, self.env.horizon, self.offline_policies, self.offline_batches,
+                batchsize        = params['batchsize'], 
+                baseline         = params['baseline'],
+                biased_offpolicy = params['biased_offpolicy'],
+                ce_batchsizes    = ce_batchsizes,
+                disc             = self.env.gamma,
+                defensive_batch  = params['defensive_batch'],
+                estimator        = params['estimator'],
+                seed             = params['seed']+n,
+                shallow          = isinstance(self.policy, ShallowGaussianPolicy),
+                stepper          = self.stepper,
+                test_batchsize   = 100)
         
+        # Onpolicy algorithm
+        elif 'onpolicy' in params['name']:
+            log = reinforce_step(
+                self.env, self.policy, self.env.horizon,
+                batchsize      = params['batchsize'], 
+                disc           = self.env.gamma,
+                stepper        = self.stepper,
+                estimator      = params['estimator'],
+                baseline       = params['baseline'],
+                shallow        = isinstance(self.policy, ShallowGaussianPolicy),
+                seed           = params['seed']+n,
+                test_batchsize = 100)
         return log
 
 if __name__ == "__main__":
     # Interactive window
-    # mysuite = MySuite(config='lq_s1.cfg', experiment='grad_comparison', numcores=1)
+    # mysuite = MySuite(config='lq_s1.cfg', numcores=1)
     
     # Command line
     mysuite = MySuite()
