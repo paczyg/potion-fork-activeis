5feab26d41e91283265898295736731c067e67ca
Giorgio <giomonline@gmail.com>
2023-02-05 19:14:26 +0100

    test: update plot swimmer results

--- experiments/learning/swimmer/showresults.py
+++ experiments/learning/swimmer/showresults.py
@@ -1,28 +1,24 @@
 #%%
 import numpy as np
 import pandas as pd
-import glob
 import scipy.stats as st
 from matplotlib import pyplot as plt
 
-import potion.visualization.notebook_utils as notebook_utils
 from experiments import plot_utils
 from expsuite import PyExperimentSuite
 
 mysuite = PyExperimentSuite(config='experiments.cfg')
 
-exps = mysuite.get_exps('results')
+exps = mysuite.get_exps('results/onpolicy')
 for exp in exps:
     exp_params = mysuite.get_params(exp)
 
     plt.figure()
-    plt.title(exp_params['name'])
+    plt.title(exp)
     plt.ylabel('Deterministic Performance')
     plt.ylabel('Return')
     plt.xlabel('Trajectories')
 
-    m, ci_lb, ci_ub = plot_utils.get_ci(mysuite, exp, 'TestPerf')
-    plt.plot(np.cumsum(mysuite.get_history(exp,0,'BatchSize')), m)
-    plt.fill_between(np.cumsum(mysuite.get_history(exp,0,'BatchSize')), ci_lb, ci_ub, alpha=.1)
+    plt.plot(mysuite.get_history(exp,0,'TestPerf'))
 
     plt.show()
