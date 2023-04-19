cfa8904d844d604f55494a4125e469a6f4069273
Giorgio Manganini <manganini@comp01.gssi.infn.it>
2023-04-19 13:48:56 +0200

    feat: reset module weights

--- potion/common/torch_utils.py
+++ potion/common/torch_utils.py
@@ -117,6 +117,24 @@ def maybe_tensor(x):
         x = torch.tensor(x)
     return x
 
+def reset_all_weights(model: torch.nn.Module, init: torch.nn.init = torch.nn.init.xavier_uniform_) -> None:
+    """
+    refs:
+        - https://discuss.pytorch.org/t/how-to-re-set-alll-parameters-in-a-network/20819/6
+        - https://stackoverflow.com/questions/63627997/reset-parameters-of-a-neural-network-in-pytorch
+        - https://pytorch.org/docs/stable/generated/torch.nn.Module.html
+    """
+
+    @torch.no_grad()
+    def weight_reset(m: torch.nn.Module):
+        weight = getattr(m, "weight", None)
+        if weight is not None:
+            init(weight)
+
+    # Applies fn recursively to every submodule see: https://pytorch.org/docs/stable/generated/torch.nn.Module.html
+    model.apply(fn=weight_reset)
+
+
 """Testing"""
 if __name__ == '__main__':
     from potion.common.mappings import LinearMapping
