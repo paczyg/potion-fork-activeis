920caac7e5761578cdc0d1ada8a0a8b2ad090268
giomgit <giomonline@gmail.com>
2023-10-24 13:16:44 +0200

    refact: plot utils

---
+++ experiments/plot_utils.py
@@ -0,0 +1,114 @@
+import os
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+from matplotlib import pyplot as plt
+from expsuite import PyExperimentSuite
+
+def plot_ci(df, key, xkey, ax=None, *plt_args, **plt_kwargs):
+    if ax is None:
+        ax=plt.gca()
+
+    stats = df.groupby(xkey)[key].agg(['mean', 'std', 'count'])
+    xs = stats.index.values
+    ys = stats['mean']
+    ci_lb, ci_ub = st.norm.interval(0.68, loc=stats['mean'], scale=stats['std'])
+    lines = ax.plot(xs,ys, *plt_args, **plt_kwargs)
+    ax.set_xticks(xs)
+    ax.fill_between(xs, ci_lb, ci_ub, alpha=.1)
+
+    return lines
+
+def get_dataframe(suite, experiment_name, xkey, cos_sim=False):
+    assert experiment_name in suite.cfgparser.sections(), \
+        "The experiment name is not present in the chosen configuration file"
+
+    experiment_path = os.path.join(suite.cfgparser.defaults()['path'], experiment_name)
+    exps = suite.get_exps(path=experiment_path)
+    params = suite.get_params(experiment_path)
+
+    df = pd.DataFrame()
+    for rep in range(params['repetitions']):
+        for exp in exps:
+            
+            _dict = suite.get_value(exp, rep, 'all', 'last')
+
+            # Use cosine similarity to compare gradients
+            if cos_sim:
+                grad_cos_sim = np.dot(_dict['grad_is'], _dict['grad_mc'])/(np.linalg.norm(_dict['grad_is'])*np.linalg.norm(_dict['grad_mc']))
+                _dict['grad_cos_sim'] = grad_cos_sim
+                del _dict['grad_is'], _dict['grad_mc']
+
+            _df = pd.DataFrame(_dict, index=[rep])
+            _df[xkey] = suite.get_params(exp)[xkey]
+            df = pd.concat([df, _df],ignore_index = True)
+    
+    return df
+
+def get_dataframes_over_repetitions(suite, exp, tags="all"):
+    # Retrieve the actual repetitions of a given experiment tags
+    # If a repetition has less iterations than the prescibed ones, NaN values are used to fill the dataframe
+
+    dfs = []
+    params = suite.get_params(exp)
+    for rep in range(params['repetitions']):
+        log = suite.get_history(exp, rep, tags)
+        if len(log) == 0:
+            continue
+        if tags != "all" and type(tags) is not list:
+            log = pd.DataFrame(log,columns=[tags])
+        else:
+            log = pd.DataFrame(log)
+        # Fill rows with the expected number of iterations
+        if log.shape[0] < params["iterations"]:
+            log = pd.concat([log,pd.DataFrame(np.nan, index=np.arange(params["iterations"]-log.shape[0]), columns=log.columns)])
+        dfs.append(log)
+    return dfs
+
+def get_ci(suite, exp, key):
+    m = suite.get_histories_over_repetitions(exp, key, np.mean)
+    s = suite.get_histories_over_repetitions(exp, key, np.std)
+    s[s==0] = "nan"
+    lb, ub = st.norm.interval(0.68, loc=m, scale=s)
+    return m, lb, ub
+
+def plot_iterations_key(dir, key='TestPerf'):
+    """
+    Plot iterations VS confidence intervals of specified key.
+    Each experiment found in the directory is plotted into a separate figure.
+
+    dir: directory with an experiment.cfg file describing the experiments in the directory
+    key: key values to plot
+    """
+    suite = PyExperimentSuite(config = os.path.join(dir,'experiment.cfg'))
+    exps = suite.get_exps(dir)
+    for exp in exps:
+
+        plt.figure()
+        plt.title(suite.get_params(exp)['name'])
+        plt.ylabel('Deterministic Performance')
+        plt.ylabel('Return')
+        plt.xlabel('Iterations')
+
+        m, ci_lb, ci_ub = get_ci(suite, exp, key)
+        plt.plot(range(len(m)), m, label='onpolicy')
+        plt.fill_between(range(len(m)), ci_lb, ci_ub, alpha=.1)
+
+        plt.show()
+
+#TODO Styling with cycler
+# https://matplotlib.org/stable/tutorials/intermediate/color_cycle.html#sphx-glr-tutorials-intermediate-color-cycle-py
+
+#TODO: un po' inutile, perchè stormpg deve caricare diversamente i dati
+# Idealmente dovrebbe servire per plottare una sezione degli esperimenti
+def plot_experiments(ax, x_key, label_keys, dir = '.'):
+    mysuite = PyExperimentSuite(config = os.path.join(dir,'experiment.cfg'))
+    exps = mysuite.get_exps(dir)
+    for exp in exps:
+        exp_params = mysuite.get_params(exp)
+        exp_str = ','.join([f"{k}={exp_params[k]}" for k in label_keys])
+
+        m, ci_lb, ci_ub = get_ci(mysuite, exp, 'TestPerf')
+        ax.plot(np.cumsum(mysuite.get_history(exp,0,x_key)), m, label=exp_str)
+        ax.fill_between(np.cumsum(mysuite.get_history(exp,0,x_key)), ci_lb, ci_ub, alpha=.1)
+    ax.legend()
